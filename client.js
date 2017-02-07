9604ad31a06127f3a544821f73b19a06e91cad2e
gwelt <sven@gwelt.net>
2017-02-07 23:52:14 +0100

    minimized

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 var ws=ws_open(location.origin.replace(/^http/, 'ws'));
 var tweets=[];
 
+function send_message(m) {if (m) {if (ws) {ws.send(m)} else {var req = new XMLHttpRequest(); req.open('GET',window.location+m); req.send();}}}
+
 function tweet(t) {
   if (t) {tweets.push(t)}
   while (tweets.length>8) {tweets.splice(0,1)}
@@ -8,6 +10,8 @@ function tweet(t) {
   if (etweet) {
     etweet.innerHTML="";
     tweets.forEach(function(tt) {etweet.innerHTML+=tt+"<br>"});
+    var form="<input id='message' type='text'><button onclick=send_message(document.getElementById('message').value)>&gt;</button>";
+    etweet.innerHTML+=form;
   };
 }
 
@@ -15,7 +19,6 @@ function ws_open(url) {
   ws=new WebSocket(url);
   ws.onmessage = function (message) {console.log(message);tweet(message.data)};
   ws.onerror = function(e) {console.log(e);tweet('CONNECTION ERROR ['+e.currentTarget.url+']')};
-  ws.onclose = function(user) {console.log(user);tweet('CONNECTION CLOSED ['+user.currentTarget.url+']')};
+  ws.onclose = function(user) {console.log(user);tweet('CONNECTION CLOSED');ws=0;};
   return ws;
 }
-
--- index.js
+++ index.js
@@ -2,27 +2,21 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const path = require('path');
-const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 const CLIENTJS = path.join(__dirname, 'client.js');
 const server = express()
-  .use('/m/:m', function(req, res) {broadcast(req.params.m); res.send('ok')})
   .use((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
     if (req.url=="/") {res.sendFile(INDEX)}
     else if (req.url=="/client.js") {res.sendFile(CLIENTJS)}
-    else if (req.query.m) {broadcast(req.query.m); res.send('ok')}
-    else {res.send('404')}
+    else {broadcast(req.url);res.send()}
   })
-  .listen(PORT, function() {process.stdout.write(`\x1b[44m SERVER LISTENING ON PORT ${ PORT } \x1b[0m `)});
+  .listen(3000);
 const wss = new SocketServer({ server });
-
-function safe_text(text) {return text.replace(/[^\w\ !]/g,'').slice(0,32)}
-///--- CONNECTION-HANDLER ---///
-function broadcast(text) {try{wss.clients.forEach((ws) => {ws.send(safe_text(text))})} catch(err){};}
 wss.on('connection', (ws) => {
   ws.send('WELCOME');
   broadcast('CLIENT CONNECTED');
   ws.on('message', (msg) => {if (msg) {broadcast(msg)}});
   ws.on('close', () => {broadcast('CLIENT DISCONNECTED')});
 });
+function safe_text(text) {return text.replace(/[^\w\ !]/g,'').slice(0,32)}
+function broadcast(text) {try{wss.clients.forEach((ws) => {ws.send(safe_text(text))})} catch(err){};}
