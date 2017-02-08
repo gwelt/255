32c4b44a2ae13d70246d627fa5b977faaf9f80b9
gwelt <sven@gwelt.net>
2017-02-08 21:46:00 +0100

    express settings

--- 255_server.js
+++ 255_server.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const server = express()
-  .use((req, res) => {
-    if (req.url=="/") {res.sendFile(require('path').join(__dirname, '255_client_simple.html'))}
-    else {broadcast(req.url);res.send()}
+  .get('/m/:m', function(req, res) {
+    broadcast(req.params.m);res.send();
+  })
+  .get('/', function(req, res) {
+    res.sendFile(require('path').join(__dirname, '255_client_simple.html'))
   })
   .listen(3000);
 const wss = new SocketServer({ server });
@@ -14,5 +16,5 @@ wss.on('connection', (ws) => {
   ws.on('message', (msg) => {if (msg) {broadcast(msg)}});
   ws.on('close', () => {broadcast('CLIENT DISCONNECTED')});
 });
-function safe_text(text) {return text.replace(/[^\w\ !]/g,'').slice(0,32)}
+function safe_text(text) {return unescape(text).replace(/[^\w\s\.,!\@#$^&%*()+=-\[\]\/{}\|:\?]/g,'').slice(0,32)} //!@#$^&%*()+=-[]\/{}|:<>?,.
 function broadcast(text) {try{wss.clients.forEach((ws) => {ws.send(safe_text(text))})} catch(err){};}
