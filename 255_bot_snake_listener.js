b5d6e570d7313a087a488e3eca8485ff65a2864f
sven <sven@gwelt.net>
2017-02-08 20:16:23 +0000

    bot_snake_listener added

---
+++ 255_bot_snake_listener.js
@@ -0,0 +1,9 @@
+const WebSocket = require('ws');
+
+ws_255=new WebSocket("ws://"+process.argv[2]);
+var ws_255_connected=false;
+ws_255.on('open', function opened() {ws_255_connected=true});
+ws_255.on('error', function(e) {console.log(e+'\nTry this: node this.js [websocket-server]:[port]');process.exit();});
+
+ws_snake=new WebSocket("ws://localhost:3000");
+ws_snake.on('message', function incoming(data, flags) {if (!data.startsWith('[') && ws_255_connected) {console.log(data);ws_255.send(data);}});
