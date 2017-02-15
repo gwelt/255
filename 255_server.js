c2258f3a617e7306e6a536defea2139a875a3bf7
gwelt <sven@gwelt.net>
2017-02-15 21:02:19 +0100

    minors

--- 255_server.js
+++ 255_server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const server = express()
+  .use(function (req, res, next) {res.setHeader('Access-Control-Allow-Origin', 'http://gwelt.net'); next();})
   .get('/m/:m', function(req, res) {broadcast(req.params.m);res.send()})
   .get('/', function(req, res) {res.sendFile(require('path').join(__dirname, '255_client_simple.html'))})
   .get('*', function(req, res) {res.send('404')})
@@ -15,5 +16,5 @@ wss.on('connection', (ws) => {
 });
 var credit=120;
 setInterval(function(){credit=120},60*60000);
-function safe_text(text) {return unescape(text).replace(/[^\w\s\däüöÄÜÖß\.,'!\@#$^&%*()+=-\[\]\/{}\|:\?]/g,'').slice(0,256)}
+function safe_text(text) {return unescape(text).replace(/[^\w\s\däüöÄÜÖß\.,'!\@#$^&%*()\+=\-\[\]\/{}\|:\?]/g,'').slice(0,256)}
 function broadcast(text) {if (credit>0) {try{wss.clients.forEach((ws) => {ws.send(safe_text(text))})} catch(err){}; credit--}}
