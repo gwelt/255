fb70780a47d281f578257577c60f92715453f315
gwelt <sven@gwelt.net>
2017-02-17 21:22:38 +0100

    reconnect

--- 255_server.js
+++ 255_server.js
@@ -2,19 +2,15 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const server = express()
-  .use(function (req, res, next) {res.setHeader('Access-Control-Allow-Origin', 'http://gwelt.net'); next();})
   .get('/m/:m', function(req, res) {broadcast(req.params.m);res.send()})
   .get('/', function(req, res) {res.sendFile(require('path').join(__dirname, '255_client_simple.html'))})
   .get('*', function(req, res) {res.send('404')})
   .listen(3000);
 const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
-  ws.send('WELCOME #'+wss.clients.length);
-  //broadcast('CLIENT CONNECTED');
+  ws.send('WELCOME #'+wss.clients.length+' (credit: '+credit+')');
   ws.on('message', (msg) => {if (msg) {broadcast(msg)}});
-  //ws.on('close', () => {broadcast('CLIENT DISCONNECTED')});
 });
-var credit=120;
-setInterval(function(){credit=120},60*60000);
+var credit=99; setInterval(function(){credit=99},60*60000);
 function safe_text(text) {return unescape(text).replace(/[^\w\s\däüöÄÜÖß\.,'!\@#$^&%*()\+=\-\[\]\/{}\|:\?]/g,'').slice(0,256)}
 function broadcast(text) {if (credit>0) {try{wss.clients.forEach((ws) => {ws.send(safe_text(text))})} catch(err){}; credit--}}
