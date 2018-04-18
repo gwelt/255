be51b1cf3334772c9920c21d7457784448cffd6b
gwelt <sven@gwelt.net>
2018-04-18 21:52:31 +0200

    local-feature

--- 255_server.js
+++ 255_server.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const server = express()
-  .get('/m/:m', function(req, res) {broadcast(req.params.m);res.send()})
-  .get('/', function(req, res) {res.sendFile(require('path').join(__dirname, '255_client_simple.html'))})
+  .get('/m/:m', function(req, res) {broadcast(req.params.m);res.send('')})
+  .get('/api/localip', function(req, res) {res.send(localip)})
+  .get('/api/golocalip', function(req, res) {res.send('<HTML><HEAD><META HTTP-EQUIV="refresh" CONTENT="0;URL=http://'+localip+':8080"></HEAD></HTML>')})
+  .get('/', function(req, res) {res.sendFile(require('path').join(__dirname,'255_client_simple.html'))})
   .get('*', function(req, res) {res.send('404')})
   .listen(3000);
 const wss = new SocketServer({ server });
 var latest_message="JUST STARTED.";
+var localip="";
 wss.on('connection', (ws) => {
   ws.send('WELCOME #'+wss.clients.length+' ('+credit+')');
   ws.on('message', (msg) => {
@@ -17,6 +20,8 @@ wss.on('connection', (ws) => {
       if (/^\/nick\ /i.test(msg)) {var n=/^\/nick\ (.*)/i.exec(msg); ws.name=safe_text(n[1]); ws.send('Welcome, '+ws.name+'.');}
       if (/^\/repeat/i.test(msg)) {ws.send(latest_message)}
       if (/^\/credit$/i.test(msg)) {credit=99;ws.send('Credit: '+credit)}
+      if (/^\/setlocalip$/i.test(msg)) {localip=ws._socket.remoteAddress.replace(/^.*:/, '');ws.send(localip)}
+      if (/^\/localip$/i.test(msg)) {ws.send(localip)}
       if (/^\/restart$/i.test(msg)) {ws.send('Ok. Restarting.');setTimeout(function(){process.exit()},3000)}
     } 
     else if (msg) {broadcast(auth+msg)}
