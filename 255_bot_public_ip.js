801fe8ddb847696581af5dad8a1b01164e012fb5
gwelt <sven@gwelt.net>
2018-04-19 23:19:04 +0200

    public_ip change

--- 255_bot_public_ip.js
+++ 255_bot_public_ip.js
@@ -1,19 +1,10 @@
 const webSocket = require('./255_ws_module').startWebsocket('publicIP',(msg,callback)=>messagehandler(msg,callback));
+var current_ip="";
 function messagehandler(data,say) {
   if (/--status/i.test(data)) {say(current_ip)}
   if (/--help/i.test(data)) {say('help: get public ip')}
-  if (/get\ public\ ip/i.test(data)) {check_ip();say('     '+current_ip)}
+  if (/get\ public\ ip/i.test(data)) {say('     '+current_ip)}
+  if (/^your\ ip\ is\ \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/i.test(data)) {var n=/^your\ ip\ is\ (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/i.exec(data); if (n[1]!=current_ip) {current_ip=n[1]; say('     '+current_ip);} }
 }
-
-var config = require('./config.json');
-var current_ip="";
-check_ip();
-setInterval(function(){check_ip()},15*60000);
-
-function check_ip() {
-  require('http').get({host:config.publicIPserver_url, port:3000, path:'/api/setlocalip'}, function(r) {
-    var res=""; 
-    r.on('data', function(d) {res+=d}); 
-    r.on('end', function() {if (current_ip!=res) {if (current_ip!="") {}; current_ip=res;}});
-  }).on('error',(e)=>{setTimeout(function(){process.exit()},60000)})
-}
+setTimeout(function(){webSocket.send('/getip')},5000);
+setInterval(function(){webSocket.send('/getip')},15*60000);
--- 255_server.js
+++ 255_server.js
@@ -6,7 +6,7 @@ const server = express()
   .get('/m/:m', function(req, res) {broadcast(req.params.m);res.send('')})
   .get('/api/setlocalip', function(req, res) {var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; localip=ip.replace(/^.*:/, ''); res.send(localip);})
   .get('/api/getlocalip', function(req, res) {res.send(localip)})
-  .get('/api/golocalip', function(req, res) {res.send('<HTML><HEAD><META HTTP-EQUIV="refresh" CONTENT="0;URL=http://'+localip+':8080"></HEAD></HTML>')})
+  .get('/api/local', function(req, res) {res.send('<HTML><HEAD><META HTTP-EQUIV="refresh" CONTENT="0;URL=http://'+localip+':8080"></HEAD></HTML>')})
   .get('/', function(req, res) {res.sendFile(require('path').join(__dirname,'255_client_simple.html'))})
   .get('*', function(req, res) {res.send('404')})
   .listen(3000);
@@ -17,15 +17,15 @@ wss.on('connection', (ws) => {
   ws.on('message', (msg) => {
     var auth="("+ws.name+") "; if (!ws.name) {auth=""};
     if (/^\//i.test(msg)) {
-      if (/^\/help/i.test(msg)) {ws.send('help: /nick [name] | /repeat | /credit | /restart')}
+      if (/^\/help/i.test(msg)) {ws.send('help: /nick [name] | /repeat | /credit | /getip | /restart')}
       if (/^\/nick\ /i.test(msg)) {var n=/^\/nick\ (.*)/i.exec(msg); ws.name=safe_text(n[1]); ws.send('Welcome, '+ws.name+'.');}
       if (/^\/repeat/i.test(msg)) {ws.send(latest_message)}
       if (/^\/credit$/i.test(msg)) {credit=99;ws.send('Credit: '+credit)}
-      if (/^\/setlocalip$/i.test(msg)) {localip=ws._socket.remoteAddress.replace(/^.*:/, '');ws.send(localip)}
-      if (/^\/localip$/i.test(msg)) {ws.send(localip)}
+      if (/^\/getip$/i.test(msg)) {ws.send('your ip is '+ws._socket.remoteAddress.replace(/^.*:/, ''))}
       if (/^\/restart$/i.test(msg)) {ws.send('Ok. Restarting.');setTimeout(function(){process.exit()},3000)}
     } 
     else if (msg) {broadcast(auth+msg)}
+    if (/^\(publicIP\)\ +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/i.test(msg)) {var n=/^\(publicIP\)\ +(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/i.exec(msg); localip = n[1];}
   });
 });
 var credit=99; setInterval(function(){credit=99},60*60000);
