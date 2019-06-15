f3533564b348fa033d3419306c4fc9a5fc412b6e
sven <sven@gwelt.net>
2019-06-16 00:28:18 +0200

    path added

--- 255_server.js
+++ 255_server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 var publicip="";
 const server = express()
-  .get('/m/:m', function(req, res) {broadcast(req.params.m);res.send('')})
-  .get('/api/setpublicip', function(req, res) {var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; publicip=ip.replace(/^.*:/, ''); res.send(publicip);})
-  .get('/api/getpublicip', function(req, res) {res.send(publicip)})
-  .get('/api/local', function(req, res) {res.send('<HTML><HEAD><META HTTP-EQUIV="refresh" CONTENT="0;URL=http://'+publicip+':8080"></HEAD></HTML>')})
-  .get('/', function(req, res) {res.sendFile(require('path').join(__dirname,'255_client_simple.html'))})
-  .get('*', function(req, res) {res.send('404')})
+  .get('/255/m/:m', function(req, res) {broadcast(req.params.m);res.send('')})
+  .get('/255/api/setpublicip', function(req, res) {var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; publicip=ip.replace(/^.*:/, ''); res.send(publicip);})
+  .get('/255/api/getpublicip', function(req, res) {res.send(publicip)})
+  .get('/255/api/local', function(req, res) {res.send('<HTML><HEAD><META HTTP-EQUIV="refresh" CONTENT="0;URL=http://'+publicip+':8080"></HEAD></HTML>')})
+  .get('/255', function(req, res) {res.sendFile(require('path').join(__dirname,'255_client_simple.html'))})
+  .get('*', function(req, res) {res.send('404 255_server')})
   .listen(3000);
 const wss = new SocketServer({ server });
 var latest_message="JUST STARTED.";
@@ -21,7 +21,6 @@ wss.on('connection', (ws) => {
       if (/^\/nick\ /i.test(msg)) {var n=/^\/nick\ (.*)/i.exec(msg); ws.name=safe_text(n[1]); ws.send('Welcome, '+ws.name+'.');}
       if (/^\/repeat/i.test(msg)) {ws.send(latest_message)}
       if (/^\/credit$/i.test(msg)) {credit=99;ws.send('Credit: '+credit)}
-      if (/^\/getip$/i.test(msg)) {ws.send(ws._socket.remoteAddress.replace(/^.*:/, ''))}
       if (/^\/restart$/i.test(msg)) {ws.send('Ok. Restarting.');setTimeout(function(){process.exit()},3000)}
     } 
     else if (msg) {broadcast(auth+msg)}
