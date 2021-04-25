8b6e407e3c702ca941981758a6f10d3b3f33b69a
sven <sven@gwelt.net>
2021-04-25 22:18:59 +0200

    new version

--- 255_printer.js
+++ 255_printer.js
@@ -14,9 +14,10 @@ socket.on('connect', function() {
 socket.on('message', function(msg,meta) {
 	if (/^--status$/i.test(msg)) {socket.emit('message','listening');}
 	if (meta&&meta.rooms) {
-		if (meta.name) {msg='('+meta.name+') '+msg};
-		if (meta.rooms.includes('#broadcast')) {ttyS0_print(msg)}
-		else if (meta.rooms.includes('#printer')) {ttyS0_print(msg,true)};
+		if (meta.rooms.includes('#broadcast')) {
+			if (meta.name) {msg='('+meta.name+') '+msg};
+			ttyS0_print(msg)
+		} else if (meta.rooms.includes('#printer')) {ttyS0_print(msg,true)};
 	}
 });
 
