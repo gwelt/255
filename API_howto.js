c8c7fd6df364e21ea9d28b70d3f3812169b0c37e
sven <sven@gwelt.net>
2021-04-27 23:12:56 +0200

    usage examples for API and socket

--- API_howto.js
+++ API_howto.js
@@ -1,6 +1,13 @@
-const fetch = require('node-fetch');
-fetch('http://localhost:3000/', {
+// use the API and send a POST-request
+const fetch = require('node-fetch')('http://localhost:3000/', {
 	method: 'post',
-	body:    JSON.stringify({ message: 'Hello World!', rooms: ['#broadcast','#test']}),
+	body:    JSON.stringify({ message: 'Hello World via API!', rooms: ['#broadcast','#test']}),
 	headers: { 'Content-Type': 'application/json' }
+});
+
+// or open a socket - and close it after sending your message
+const socket = require('socket.io-client')('http://localhost:3000');
+socket.on('connect', function() {
+	socket.emit('message','Hello World via socket!',{rooms:['#broadcast','#test']});
+	setTimeout(function(){socket.disconnect()},100);
 });
