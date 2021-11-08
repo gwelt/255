b2d6dc4c88e7ba12c44d37d9f0e8adfdef33e943
sven <sven@gwelt.net>
2021-11-08 22:55:38 +0100

    print ID

--- 255_printer.js
+++ 255_printer.js
@@ -5,7 +5,7 @@ ttyS0_print("================================\\nIP: "+require('os').networkInter
 
 socket.on('connect', function() {
 	console.log(new Date().toISOString()+' | '+socket.id);
-	ttyS0_print(get_time()+' CONNECTED');
+	socket.emit('message',socket.id,{rooms:[socket.id]});
 	socket.emit('name','printer');
 	socket.emit('join',['#printer'].concat(Array.isArray(config.printer_rooms)?config.printer_rooms:[]));
 	socket.emit('info','Messages to #printer will output on this thermal printer. Usage: /m #printer [text]');
